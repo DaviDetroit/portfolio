9eb17a65f0484ddc0781fca12c692cdbedecfb02
unknown <davidetroitff11@gmail.com>
2026-07-13 11:09:35 -0300

    Teste

--- backend/db.php
+++ backend/db.php
@@ -6,18 +6,18 @@ if (file_exists(__DIR__ . '/../.env')) {
     Dotenv\Dotenv::createImmutable(__DIR__ . '/../')->load();
 }
 
-$host = $_ENV['DB_HOST'];
-$port = $_ENV['DB_PORT'];
-$db   = $_ENV['DB_NAME'];
-$user = $_ENV['DB_USER'];
-$pass = $_ENV['DB_PASSWORD'];
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$db   = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASSWORD');
 
 $pdo = new PDO(
     "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
     $user,
     $pass,
     [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_ERRMODE => PDO::ATTR_ERRMODE,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]
 );
