f2c7d0e35f5b61c93a19ef053a13c7f2ea86ea54
unknown <davidetroitff11@gmail.com>
2026-07-13 11:15:46 -0300

    Debuggando

--- backend/db.php
+++ backend/db.php
@@ -1,23 +1,29 @@
 <?php
 
-declare(strict_types=1);
-
-if (file_exists(__DIR__ . '/../.env')) {
-    Dotenv\Dotenv::createImmutable(__DIR__ . '/../')->load();
-}
-
 $host = getenv('DB_HOST');
 $port = getenv('DB_PORT');
 $db   = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASSWORD');
 
-$pdo = new PDO(
-    "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
-    $user,
-    $pass,
-    [
-        PDO::ATTR_ERRMODE => PDO::ATTR_ERRMODE,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ]
-);
+
+
+if (!$host || !$port || !$db || !$user || !$pass) {
+    die("Erro: variáveis de ambiente do banco não estão definidas!");
+}
+
+try {
+    // Criando conexão PDO
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+        $user,
+        $pass
+    );
+    
+    // Configura o PDO para lançar exceções em caso de erro
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    // Mensagem de erro caso a conexão falhe
+    die("Erro de conexão. Tente novamente mais tarde.");
+}
